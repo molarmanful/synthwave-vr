2619613fffff3d1718d2280d47a6f40f114e7682
Benjamin Pang <molarmanful@users.noreply.github.com>
2020-02-16 03:33:28 +0400

    major update 2

---
+++ Assets/Playlist.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playlist : MonoBehaviour {
+
+  Object[] myMusic;
+
+  void Awake(){
+    myMusic = Resources.LoadAll("Music", typeof(AudioClip));
+    GetComponent<AudioSource>().clip = myMusic[0] as AudioClip;
+  }
+
+  void Start(){
+    playRandomMusic();
+  }
+
+  void Update(){
+    if(!GetComponent<AudioSource>().isPlaying || Input.GetMouseButtonDown(0))
+      playRandomMusic();
+  }
+
+  void playRandomMusic(){
+    GetComponent<AudioSource>().clip = myMusic[Random.Range(0, myMusic.Length)] as AudioClip;
+    GetComponent<AudioSource>().Play();
+  }
+}
---
+++ Assets/Texture_Scroll.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Texture_Scroll : MonoBehaviour {
+
+  float scrollSpeed = 0.3529f;
+  Renderer rend;
+
+  void Start(){
+    rend = GetComponent<Renderer>();
+  }
+
+  void Update(){
+    float offset = Time.time * -scrollSpeed;
+    rend.material.mainTextureOffset = new Vector2(0, offset);
+  }
+}
